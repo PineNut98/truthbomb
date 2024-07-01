428d811e45f1827c1e29a9a19e35ff258c00cc3f
PineNut98 <149560462+PineNut98@users.noreply.github.com>
2024-07-02 02:57:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("mousemove", function(event) {
         var follower = document.getElementById("follower");
-        follower.style.left = (event.pageX + 10) + "px"; // Offset to center the image horizontally
-        follower.style.top = (event.pageY + 10) + "px"; // Offset to center the image vertically
+        follower.style.left = (event.pageX - 25) + "px"; // Offset to center the image horizontally
+        follower.style.top = (event.pageY - 25) + "px"; // Offset to center the image vertically
     });
 });
