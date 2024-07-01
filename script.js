d9b1587f2834bd22f258973cf3a0a6c7f804218b
PineNut98 <149560462+PineNut98@users.noreply.github.com>
2024-07-02 03:01:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("mousemove", function(event) {
         var follower = document.getElementById("follower");
+        follower.style.left = event.pageX + "px";
+        follower.style.top = event.pageY + "px";
     });
 });
