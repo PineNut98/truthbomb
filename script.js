30a8c6eb66fac08723650815d2bd2b9926bde11a
PineNut98 <149560462+PineNut98@users.noreply.github.com>
2024-07-02 03:21:40 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    var follower = document.getElementById('follower');
-    var xOffset = -30; // Adjust these values as needed
-    var yOffset = -30; // Adjust these values as needed
-    
-    follower.style.top = (e.clientY + yOffset) + 'px';
-    follower.style.left = (e.clientX + xOffset) + 'px';
-});
